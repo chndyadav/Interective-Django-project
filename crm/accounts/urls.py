86ac549600003970fcd65602674f5604dc888a93
Chandrakant <chndyadav@gmail.com>
2020-06-16 22:53:37 +0530

    commiting further changes

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -3,9 +3,17 @@ from . import views
 
 
 urlpatterns = [
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
+
     path('', views.home, name="home"),
-    path('products/', views.products, name="product"),
-    path('customer/<str:pk>/', views.customer, name="customer"),
+    path('user/', views.userPage, name="user-page"),
+
+    path('account/', views.accountSettings, name="account"),
+
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
 
     path('create_order/<str:pk>/', views.createOrder, name="create_order"),
     path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
